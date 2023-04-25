1c5b51cf956f964944e06bce7b0b41594af81013
Александр Гармаш <garmash_a@itstep.academy>
2023-04-25 18:53:50 +0300

    Reference

---
+++ ConsoleApplication23/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Задача 1. Створити функцію, що приймає посилання на змінну з цілим числом та змінює
+// це число поділивши його напвіл
+
+int f1(int a) {
+    return a / 2;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
